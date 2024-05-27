68129eab0949e9afd713ebdc3cc4f3d8e2d29619
hoch12 <161817511+hoch12@users.noreply.github.com>
2024-05-27 12:37:11 +0200

    commit

--- src/Game.java
+++ src/Game.java
@@ -2,5 +2,6 @@ import java.util.Scanner;
 
 public abstract class Game {
 
+
     public abstract void play(User user, Scanner scanner);
 }
--- src/GameException.java
+++ src/GameException.java
@@ -1,5 +1,9 @@
 public class GameException extends Exception{
 
+    /**
+     * Writes a message for a exception
+     * @param message
+     */
     public GameException(String message) {
         super(message);
     }
